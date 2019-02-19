5604f5ee1eedc3ba25c585da8f2d85e1e1288476
guyomh <guyomh@gmail.com>
2019-02-20 00:10:53 +0100

    Mise à jour partie Admin (Navigation)

--- admin/index.php
+++ admin/index.php
@@ -10,17 +10,45 @@
     <link rel="shortcut icon" href="img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   </head>
   <body>
 
     <div id="layout">
       <nav>
         <img src="img/logo_grand_angle.png" alt="logo Grand Angle" id="logo">
+        <ul>
+          <li><a href="index.php" title="Connexion / Déconnexion">CONNEXION</a></li>
+          <li>
+            <div class="menu-titre">EXPOSITIONS</div>
+            <ul>
+              <li><a href="liste-expo.php" title="Liste des expositions">Liste</a></li>
+              <li><a href="editer-expo.php" title="Créer ou éditer une exposition">Créer</a></li>
+            </ul>
+          </li>
+          <li>
+            <div class="menu-titre">GESTION</div>
+            <ul>
+              <li><a href="editer-oeuvre.php" title="Créer ou éditer une oeuvre">Oeuvres</a></li>
+              <li><a href="editer-artiste.php" title="Créer ou éditer un artiste">Artistes</a></li>
+              <li><a href="editer-collectif.php" title="Créer ou éditer un collectif">Collectifs</a></li>
+            </ul>
+          </li>
+          <li>
+            <div class="menu-titre">COLLABORATEURS</div>
+            <ul>
+              <li><a href="liste-collab.php" title="Liste des collaborateurs">Liste</a></li>
+              <li><a href="editer-collab.php" title="Créer ou éditer un collaborateur">Ajouter</a></li>
+            </ul>
+          </li>
+          <li><a href="traduction.php" title="Trouver les contenus non traduits">TRADUCTIONS</a></li>
+          <li><a href="statistique.php" title="Statistiques de consultation de la partie publique du site">STATISTIQUES</a></li>
+        </ul>
       </nav>
       <div id="header-main">
-        <div id="header-container">
+        <div id="header-bkg">
           <header>
-            <h1>►&nbsp;CONNEXION</h1>
+            <h1>► CONNEXION</h1>
           </header>
         </div>
         <main>
