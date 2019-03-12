4835f44a73885636a2b139b51c98844b2902b871
guyomh <guyomh@gmail.com>
2019-03-12 09:54:47 +0100

    Gestion par défaut de la valeur du cookie de langue dans les fonctions utiles

--- visiteurs/inc/func.inc.php
+++ visiteurs/inc/func.inc.php
@@ -5,6 +5,10 @@ function frenchDate($date)
   return date("d/m/Y", strtotime($date));
 }
 
+// Gestion de la valeur par défaut du $_COOKIE
+if(!isset($_COOKIE['lang'])) { $_COOKIE['lang'] = "fr"; }
+
+// Gestion du paramètre ID
 if (isset($_GET['id']))
 {
   $param = "&id=" . intval($_GET['id']);
