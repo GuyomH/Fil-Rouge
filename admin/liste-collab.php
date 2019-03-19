8d0f7cfa8abcdbf64966529825d9cf787033afa4
Florent <florent19du86@gmail.com>
2019-03-19 16:41:26 +0100

    page liste collab

--- admin/liste-collab.php
+++ admin/liste-collab.php
@@ -22,8 +22,38 @@ require_once('inc/nav.inc.php');
 /****************************/
 /* LISTE DES COLLABORATEURS */
 /****************************/
-// Code spécifique de la page
+// Liste des collaborateurs
+
+// if (isset($_GET['del']) && strlen($_GET['del'])==3)
+// {
+//   $del=$_GET['del'];
+//   $requete1="DELETE  FROM collaborateurs WHERE id_co = '$del'";
+//   $db->exec($requete1);
+//   header('Location:liste-collab.php');
+//   exit();
+// }
+//<a href=\"liste-collab.php?del={$trigramme}\" title=\"Supprimer le collaborateur\"><button>Supprimer</button></a>
+
+  $listCollab = "";
+  $requete2="SELECT nom_co, prenom_co, email_co, id_co FROM collaborateurs";
+  $reponse=$db->query($requete2);
+
+  foreach ($reponse as $info)
+  {
+    $nom  = $info['nom_co'];
+    $prenom = $info['prenom_co'];
+    $email = $info['email_co'];
+    $trigramme = $info['id_co'];
+
+    $listCollab .= "<tr><td>$nom</td><td>$prenom</td><td>$email</td><td>$trigramme</td><td class='list'><a href=\"editer-collab.php?collab={$trigramme}\" title=\"Edition le collaborateur\"><button>Éditer</button></a></td></tr>\r\n";
+
+  }
 ?>
 <?php require_once('inc/head.inc.php'); ?>
           <!-- FRONT DE LA PAGE -->
+          <h2>Liste des collaborateurs</h2>
+          <table class="list-collab">
+            <tr><th>Nom</th><th>Prénom</th><th>Email</th><th>Trigramme</th></tr>
+            <?php echo $listCollab; ?>
+          </table>
 <?php require_once('inc/foot.inc.php'); ?>
