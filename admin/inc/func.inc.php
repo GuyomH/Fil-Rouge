7d13bce37d5711b15d98a75d5a8e139e5e78b84a
guyomh <guyomh@gmail.com>
2019-03-20 08:26:45 +0100

    Générateur de mots de passe

--- admin/inc/func.inc.php
+++ admin/inc/func.inc.php
@@ -14,4 +14,20 @@ function dateChecker($date)
   $day = $split[2];
   return checkdate ($month, $day, $year);
 }
+
+// Générateur de mots de passe
+function pwdGen()
+{
+  $chars = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+   "t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M",
+  "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6",
+  "7","8","9","&","#","@","+","-","*","!","=","$","%",".",",",";");
+  $indexMax = count($chars) - 1;
+  $pwd = "";
+  for($i = 0 ; $i < 10; $i++)
+  {
+    $pwd .= $chars[rand(0,  $indexMax)];
+  }
+  return $pwd;
+}
 ?>
