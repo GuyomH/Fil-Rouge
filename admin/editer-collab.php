0b2650d8fbff6b96e68e6bd5c0896df56f81fa23
Florent <florent19du86@gmail.com>
2019-03-22 13:31:43 +0100

    commencement page editer collab

--- admin/editer-collab.php
+++ admin/editer-collab.php
@@ -27,4 +27,14 @@ require_once('inc/nav.inc.php');
 ?>
 <?php require_once('inc/head.inc.php'); ?>
           <!-- FRONT DE LA PAGE -->
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>" class="wide">
+    <p><label for="nom">Nom* :</label> <input type="text" id="nom" name="nom" maxlength="500" value=""></p>
+    <p><label for="prenom">Prénom* :</label> <input type="text" id="prenom" name="prenom" maxlength="500" value=""></p>
+    <p><label for="email">Email :</label> <input type="text" id="email" name="email" maxlength="500" value=""></p>
+    <p><label for="trigramme">Trigramme :</label> <input type="text" id="trigramme" name="trigramme" maxlength="500" value=""></p>
+    <p>
+      <input type="submit" name="editerCollaborrateur" id="editCollab" value="Valider">
+      <input type="submit" name="editerCollaborrateur-reset" id="editCollab-reset" value="Annuler">
+    </p>
+  </form>
 <?php require_once('inc/foot.inc.php'); ?>
