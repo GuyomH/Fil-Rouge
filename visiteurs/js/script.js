16ec8ba9ec55ede07872993dc3016ae630808a3e
guyomh <guyomh@gmail.com>
2019-07-08 14:04:50 +0200

    Maj script avertissement

--- visiteurs/js/script.js
+++ visiteurs/js/script.js
@@ -14,11 +14,13 @@ $( ".toggle" ).click(function() {
 });
 });
 
+
 //https://developer.mozilla.org/fr/docs/Web/API/Document/cookie
 var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 var path = window.location.pathname;
 var page = path.split("/").pop();
 if(page == "index.php" && cookieValue == "")
 {
-  alert('Cet applicatif est destiné à être visionné sur place avec un smartphone ou une phablette pas les visiteurs de la galerie d\'art Grand Angle');
+  alert('Cet applicatif est destiné à être visionné sur place avec un smartphone ou une phablette par les visiteurs de la galerie d\'art Grand Angle');
+  document.cookie = "lang=fr";
 }
